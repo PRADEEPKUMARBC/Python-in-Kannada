45001a86811c37116e2082a8aea6437e806ce1cf
PRADEEPKUMAR B C <pradeepkumarbc138@gmail.com>
2026-02-13 22:19:24 +0530

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,46 @@
+Name  = "Pradeep"
+age = 22
+is_student = True
+weight = 70.5
+
+print("Name:", Name)
+print("Age:", age)
+print("Is Student:", is_student)
+print(type(Name))
+print(type(age))
+print(type(is_student))
+print(type(weight))
+
+a = 10
+b = 3
+
+addition = a + b
+print("Addition:", addition)
+
+subtraction = a - b
+print("Subtraction:", subtraction)
+
+multiplication = a * b
+print("Multiplication:", multiplication)
+
+division = a / b
+print("Division:", division)
+
+floor_divison = a / b
+print("Floor Division:", floor_divison)
+
+modulus = a % b
+print("Modulus:", modulus)
+
+exponentiation = a ** b
+print("Exponentiation:", exponentiation)
+
+a = 10
+b = 20
+
+c = a
+a = b
+b = c
+
+print("Value of a:", a)
+print("Value of b:", b)
